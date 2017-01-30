1107164efda030051b8e19e1aeca75ea854b080d
David Horsley <david.horsley@nviinc.com>
2017-01-30 15:18:17 -0500

    moved attach to main package

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
-package main
+package fs
 
 import (
+	"bufio"
 	"fmt"
-	"log"
+	"os"
+	"regexp"
+	"strconv"
 
 	versions "fs/versions"
 	_ "fs/versions/all"
@@ -10,11 +13,46 @@ import (
 
 type FieldSystem versions.FieldSystem
 
-func main() {
-	fs, err := versions.Attach()
+func Attach() (FieldSystem, error) {
+	vers, err := InstalledVersion()
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
-	fmt.Println(fs.Version())
-	fmt.Println(fs.Log())
+	creator, ok := versions.Creators[vers]
+	if !ok {
+		return nil, fmt.Errorf("error: version %s not supported", vers)
+	}
+	fs, err := creator()
+	return fs, err
+
+}
+
+func InstalledVersion() (string, error) {
+	r, err := regexp.Compile(`^(\w+)\s*=\s*(\w+)$`)
+	if err != nil {
+		return "", err
+	}
+
+	vars := make(map[string]int)
+
+	f, err := os.Open(basepath + "/Makefile")
+	if err != nil {
+		return "", err
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		m := r.FindStringSubmatch(line)
+		if m == nil {
+			continue
+		}
+		i, err := strconv.Atoi(m[2])
+		if err != nil {
+			continue
+		}
+		vars[m[1]] = i
+	}
+	return fmt.Sprintf("%d.%d.%d", vars["VERSION"], vars["SUBLEVEL"], vars["PATCHLEVEL"]), nil
 }
