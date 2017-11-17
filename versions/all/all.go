54dba467aaf4d4c5734c579a77acd04de4408d97
David Horsley <david.horsley@nviinc.com>
2017-11-17 18:40:12 +0000

    added fs9.12.12

--- versions/all/all.go
+++ versions/all/all.go
@@ -1,12 +1,11 @@
 package all
 
 import (
-	// _ "../fs1"
-	// _ "../fs2"
 	_ "fs/versions/fs9.11.19"
 	_ "fs/versions/fs9.11.8"
 	_ "fs/versions/fs9.12.10"
 	_ "fs/versions/fs9.12.11"
+	_ "fs/versions/fs9.12.12"
 	_ "fs/versions/fs9.12.8"
 	_ "fs/versions/fs9.12.9"
 )
